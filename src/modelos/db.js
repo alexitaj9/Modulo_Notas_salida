e55aac50edbc5f10faa43c52147a2b2d07d28039
DuwanVargasSalas96 <dubansalas@outlook.com>
2021-11-21 18:59:05 -0500

    Desarrollo del core de la aplicación.

---
+++ src/modelos/db.js
@@ -0,0 +1,32 @@
+//Create const
+const mongodb = require("mongodb");
+const mongodbClient = mongodb.MongoClient;
+const uri = "mongodb+srv://<usuario>:<password>@mintic.9z7mi.mongodb.net/?retryWrites=true&w=majority";
+const databaseName = "notas_de_salida";
+const collectionName = "test";
+
+//Connect to database
+function connection(successCallBack, failureCallBack) {
+    //Execute connection
+    mongodbClient.connect(uri, function (error, databaseInstance) {
+        //Connection control
+        if(error) {
+            //Log
+            console.log("MongoDB error: " + error)
+        }
+        else {
+            //Create const
+            const database = databaseInstance.db(databaseName);
+            const collection = database.collection(collectionName);
+            
+            //Log
+            console.log("MongoDB information: Connection done");
+
+            //Callback
+            successCallBack(collection);
+        }
+  });
+}
+
+//Exports
+module.exports = { connection };
